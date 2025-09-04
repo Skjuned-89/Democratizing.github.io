6511eae81ae4c353b72af353ca7291c61187173a
Sk Juned <148184794+Skjuned-89@users.noreply.github.com>
2025-09-04 11:01:55 +0530

    Create swiperScript.js

---
+++ swiperScript.js
@@ -0,0 +1,38 @@
+var swiper = new Swiper(".mySwiper", {
+  spaceBetween: 15,
+  loop: true,
+  slidesPerView: 'auto',      
+  speed: 4000,               
+  autoplay: {
+    delay: 0,                
+    disableOnInteraction: false,
+  },
+  freeMode: true,           
+  freeModeMomentum: false,     
+  allowTouchMove: false,     
+  breakpoints: {
+    0: {
+      slidesPerView: 2,
+    },
+    1024: {
+      slidesPerView: 3,
+    },
+  },
+});
+function applySwiper(className){
+  let swipers = new Swiper(`.${className}`, {
+    loop: true,
+    slidesPerView: 'auto',      
+    spaceBetween: 30,
+    speed: 20000,               
+    autoplay: {
+      delay: 0,                
+      disableOnInteraction: false,
+    },
+    freeMode: true,           
+    freeModeMomentum: false,     
+    allowTouchMove: false,     
+  });
+}
+applySwiper("swiper1")
+applySwiper("swiper2")
