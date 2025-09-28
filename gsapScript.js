eefc18ab14727afa51fb160773333662af51f88b
Sk Juned <148184794+Skjuned-89@users.noreply.github.com>
2025-09-28 11:26:53 +0530

    Update gsapScript.js

--- gsapScript.js
+++ gsapScript.js
@@ -1,4 +1,4 @@
-let gsapAnimation=(animationValue,triggerValue)=>{
+let gsapAnimation = (animationValue, triggerValue) => {
     gsap.from(animationValue, {
         y: 100,
         opacity: 0,
@@ -22,8 +22,8 @@ navLi.forEach((list, listIndex) => {
                 gsap.from(cardsElem, {
                     y: -50,
                     opacity: 0,
-                    duration: 1,
-                    stagger: .1,
+                    duration: 0.7,
+                    stagger: 0.1,
                     ease: "power1.Out"
                 })
             }
@@ -48,35 +48,35 @@ navLi.forEach((list, listIndex) => {
         })
     })
 })
-gsapAnimation(".realEstateSection .container h2",".realEstateSection .container")
+gsapAnimation(".realEstateSection .container h2", ".realEstateSection .container")
 let realEstateCards = document.querySelectorAll(".realEstateCard")
 realEstateCards.forEach((realEstateCard) => {
-    gsapAnimation(realEstateCard,realEstateCard)
+    gsapAnimation(realEstateCard, realEstateCard)
 })
-gsapAnimation(".howWorksAccordionSection h1",".howWorksAccordionSection")
+gsapAnimation(".howWorksAccordionSection h1", ".howWorksAccordionSection")
 let accordionTitals = document.querySelectorAll(".accordionTital")
 accordionTitals.forEach((accordionTital) => {
-    gsapAnimation(accordionTital,accordionTital)
+    gsapAnimation(accordionTital, accordionTital)
 })
 let accordionContents = document.querySelectorAll(".accordionContent")
 accordionContents.forEach((accordionContent) => {
-    gsapAnimation(accordionContent,accordionContent)
+    gsapAnimation(accordionContent, accordionContent)
 })
-gsapAnimation(".accordionLowerContent p",".accordionLowerContent p")
-gsapAnimation(".investmentSection h1",".investmentSection h1")
-let investmentCards=document.querySelectorAll(".investmentCard")
-investmentCards.forEach(investmentCard=>{
-    gsapAnimation(investmentCard,investmentCard)
+gsapAnimation(".accordionLowerContent p", ".accordionLowerContent p")
+gsapAnimation(".investmentSection h1", ".investmentSection h1")
+let investmentCards = document.querySelectorAll(".investmentCard")
+investmentCards.forEach(investmentCard => {
+    gsapAnimation(investmentCard, investmentCard)
 })
-gsapAnimation(".mortgageSolutionSection .container h1",".mortgageSolutionSection .container h1")
-gsapAnimation(".clientTop h1",".clientTop h1")
-gsapAnimation(".clientButtom .container h2",".clientButtom .container h2")
-gsapAnimation(".featuredSection .container h2",".featuredSection .container h2")
-gsapAnimation(".buildWealthSection .container h2",".buildWealthSection .container h2")
-gsapAnimation(".buildWealthSection .container p",".buildWealthSection .container p")
-gsapAnimation(".informationSection h1",".informationSection h1")
+gsapAnimation(".mortgageSolutionSection .container h1", ".mortgageSolutionSection .container h1")
+gsapAnimation(".clientTop h1", ".clientTop h1")
+gsapAnimation(".clientButtom .container h2", ".clientButtom .container h2")
+gsapAnimation(".featuredSection .container h2", ".featuredSection .container h2")
+gsapAnimation(".buildWealthSection .container h2", ".buildWealthSection .container h2")
+gsapAnimation(".buildWealthSection .container p", ".buildWealthSection .container p")
+gsapAnimation(".informationSection h1", ".informationSection h1")
 gsapAnimation()
-let imgactive=document.querySelectorAll(".imageSection .imgactive")
-imgactive.forEach(active=>{
-    gsapAnimation(active,active)
+let imgactive = document.querySelectorAll(".imageSection .imgactive")
+imgactive.forEach(active => {
+    gsapAnimation(active, active)
 })
